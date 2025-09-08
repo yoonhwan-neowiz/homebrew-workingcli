3cb2250e5353f8b86ca980c0d9e8d7706d7a6e50
yoonhwan.ko <yoonhwan.ko@gamfs.net>
2025-09-08 17:30:54 +0900

    feat: Release version 0.1.1
    - Updated Formula to version 0.1.1
    - Updated download URLs
    - Updated SHA256 checksums

--- Formula/ga.rb
+++ Formula/ga.rb
@@ -1,16 +1,16 @@
 class Ga < Formula
   desc "Git Assistant - Smart Git workflow optimizer for large repositories"
   homepage "https://github.com/yoonhwan-neowiz/WorkingCli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
   
   # macOS 플랫폼별 설정
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.0/ga-darwin-arm64.tar.gz"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.1/ga-darwin-arm64.tar.gz"
       sha256 "055857e9fd878764b4e660a554b91b073acb6a8d2c4c4ff71ee545a4e471ea62"  # release.sh가 자동 업데이트
     else
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.0/ga-darwin-amd64.tar.gz"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.1/ga-darwin-amd64.tar.gz"
       sha256 "9b4ee989d0f1b1a441368f97d4c7af680d7f964295115dabcc98862b56340cc5"  # release.sh가 자동 업데이트
     end
   end
@@ -18,10 +18,10 @@ class Ga < Formula
   # Linux 플랫폼별 설정
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.0/ga-linux-arm64.tar.gz"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.1/ga-linux-arm64.tar.gz"
       sha256 "18574392309448578c60a87789092fb73865db0084bfd73fbaf5fb5f9ca520e7"  # release.sh가 자동 업데이트
     else
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.0/ga-linux-amd64.tar.gz"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.1.1/ga-linux-amd64.tar.gz"
       sha256 "075fc38c200857047ef9b1f1c03d3814098b102a4557247cd0c678770a585c53"  # release.sh가 자동 업데이트
     end
   end
