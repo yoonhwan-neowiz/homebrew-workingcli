7961fa5f14e52e6e32ca1dd4ad00e0a105940174
yoonhwan.ko <yoonhwan.ko@gamfs.net>
2025-09-16 18:57:21 +0900

    feat: Release version 0.2.2
    - Updated Formula to version 0.2.2
    - Updated download URLs
    - Updated SHA256 checksums for all platforms

--- Formula/ga.rb
+++ Formula/ga.rb
@@ -1,28 +1,28 @@
 class Ga < Formula
   desc "Git Assistant - Smart Git workflow optimizer for large repositories"
   homepage "https://github.com/yoonhwan-neowiz/WorkingCli"
-  version "0.2.1"
+  version "0.2.2"
   license "MIT"
   
   # macOS 플랫폼별 설정
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.1/ga-darwin-arm64.tar.gz"
-      sha256 "b6410588b461e53ac15bcf76c9d25ffea1ce2b2f52aec84eb6e9a932de45fbee"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.2/ga-darwin-arm64.tar.gz"
+      sha256 "f24cc0ac70ca303ecb619a035291c372b38dfa584919db3e4c15cab94bf12738"
     else
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.1/ga-darwin-amd64.tar.gz"
-      sha256 "2987af74994cbe88ddd3a138e5ed4631998b5a1e5fa2146bb1768a021674b5c6"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.2/ga-darwin-amd64.tar.gz"
+      sha256 "6ba0bca070e58a17b1d32b957c70cd13cee14c5fd2e72c2f65c8a4cb0f2446d4"
     end
   end
 
   # Linux 플랫폼별 설정
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.1/ga-linux-arm64.tar.gz"
-      sha256 "c6aad07c0e85e4d581b998f4cea1cac2e5af516d699e358942c4942e09b889ed"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.2/ga-linux-arm64.tar.gz"
+      sha256 "b92ad54b61aec050ce999ebaf3c0062a938cd720ec350ffbef1aa75862d6f752"
     else
-      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.1/ga-linux-amd64.tar.gz"
-      sha256 "addd1d6413e0db8d32f6911e2b16085fa5c259fa205a273bd2f039f6aefcdad6"
+      url "https://github.com/yoonhwan-neowiz/homebrew-workingcli/releases/download/v0.2.2/ga-linux-amd64.tar.gz"
+      sha256 "dc8ee39f50ec4a2b20484225c6485b1c58ad411ea38f36b33ba6fd8491ff9e83"
     end
   end
 
